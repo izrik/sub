61bdc939177b60054557eae844406257ad47dd59
izrik <izrik@izrik.com>
2014-03-18 10:34:59 -0500

    Use NCommander.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NDesk.Options;
 using Substrate;
+using NCommander;
+using System.Reflection;
 
 namespace sub
 {
@@ -30,74 +32,26 @@ namespace sub
 
                 args = _options.Parse(args).ToArray();
 
+                var commander = new Commander("sub", GetVersionStringFromAssembly());
+                commander.AdditionalUsage = () => {
+                     Console.WriteLine("Options:");
+                     Console.WriteLine();
+                     _options.WriteOptionDescriptions(Console.Out);
+                     Console.WriteLine();
+                };
 
-                if (showHelp || args.Length < 1)
+                if (showVersion)
                 {
-                    ShowUsage();
+                    commander.ShowVersion();
                     return;
                 }
-                else if (showVersion)
+                else if (showHelp || args.Length < 1)
                 {
-                    ShowVersion();
+                    commander.ShowUsage();
                     return;
                 }
 
-                var command = args[0].ToLower();
-
-                if (command == "help")
-                {
-                    ShowUsage();
-                }
-                else if (command == "clear")
-                {
-                    var folder = args[1];
-
-                    AnvilWorld world = AnvilWorld.Open(folder);
-                    BlockManager bm = world.GetBlockManager();
-
-                    int minx = int.Parse(args[2]);
-                    int maxx = int.Parse(args[3]);
-                    int miny = int.Parse(args[4]);
-                    int maxy = int.Parse(args[5]);
-                    int minz = int.Parse(args[6]);
-                    int maxz = int.Parse(args[7]);
-
-                    bm.ClearSpace(minx, maxx, miny, maxy, minz, maxz);
-
-                    world.Save();
-                }
-                else if (command == "fill")
-                {
-                    var folder = args[1];
-
-                    AnvilWorld world = AnvilWorld.Open(folder);
-                    BlockManager bm = world.GetBlockManager();
-
-                    int minx = int.Parse(args[2]);
-                    int maxx = int.Parse(args[3]);
-                    int miny = int.Parse(args[4]);
-                    int maxy = int.Parse(args[5]);
-                    int minz = int.Parse(args[6]);
-                    int maxz = int.Parse(args[7]);
-
-                    int blockType = int.Parse(args[8]);
-
-                    if (args.Length > 9)
-                    {
-                        int data = int.Parse(args[9]);
-                        bm.FillSpace(minx, maxx, miny, maxy, minz, maxz, blockType, data);
-                    }
-                    else
-                    {
-                        bm.FillSpace(minx, maxx, miny, maxy, minz, maxz, blockType);
-                    }
-
-                    world.Save();
-                }
-                else
-                {
-                    Console.WriteLine("Unknown command, \"{0}\"", command);
-                }
+                commander.ProcessArgs(args);
             }
             catch (Exception ex)
             {
@@ -105,7 +59,7 @@ namespace sub
                 if (verbose)
                 {
                     Console.WriteLine(":");
-                    Console.WriteLine(ex.ToString());
+                    Console.WriteLine(ex);
                 }
                 else
                 {
@@ -114,27 +68,9 @@ namespace sub
             }
         }
 
-        static void ShowVersion()
+        public static string GetVersionStringFromAssembly()
         {
-            Console.WriteLine("sub.exe version x.y.z");
-        }
-
-        static void ShowUsage()
-        {
-            Console.WriteLine("Usage:");
-            Console.WriteLine("    sub [options]");
-            Console.WriteLine("    sub help");
-            Console.WriteLine("    sub clear [folder] [args]");
-            Console.WriteLine("    sub fill [folder] [args]");
-            Console.WriteLine();
-            Console.WriteLine("Subcommands:");
-            Console.WriteLine();
-            Console.WriteLine("    help,           Help");
-            Console.WriteLine("    clear,          Clear a space");
-            Console.WriteLine("    fill,           Fill a space");
-            Console.WriteLine();
-
-            _options.WriteOptionDescriptions(Console.Out);
+            return Assembly.GetCallingAssembly().GetName().Version.ToString();
         }
     }
 }
