cbc0322ae62e47fc85a2facce293f1e7d1593727
izrik <izrik@izrik.com>
2014-03-20 12:32:07 -0500

    Compact the list of block types. Also show numbers.

--- ParameterTypes.cs
+++ ParameterTypes.cs
@@ -15,19 +15,47 @@ namespace sub
                 arg = arg.ToLower();
                 if (BlockTypesByName._BlockTypesByName.ContainsKey(arg))
                 {
-                return BlockTypesByName._BlockTypesByName[arg];
+                    return BlockTypesByName._BlockTypesByName[arg];
                 }
     
                 return int.Parse(arg);
             },
             description: "The name or number of a kind of block, e.g., air, stone, dirt",
-            helpText: "The available block types are:\r\n\r\n" + 
-                string.Join(
-                    "\r\n", 
-                    BlockTypesByName._BlockTypesByName.Keys.Select(
-                        name => string.Format("  {0}", name))),
+            helpText: "The available block types are:\r\n\r\n" + GetListOfBlockTypes(),
             outputType: typeof(int)
         );
+
+        static string GetListOfBlockTypes()
+        {
+            var sb = new StringBuilder();
+            string line = "    ";
+
+            var blockTypeNames = BlockTypesByName._BlockTypesByName.Keys.ToList();
+            blockTypeNames.Sort();
+
+            int i = 1;
+            foreach (var name in blockTypeNames)
+            {
+                bool last = (i == (blockTypeNames.Count));
+
+                var entry = string.Format(
+                                "{0} ({1}){2}",
+                                name,
+                                BlockTypesByName._BlockTypesByName[name],
+                                (last ? "" : ", "));
+
+                if (line.Length + entry.Length + 1 > 79)
+                {
+                    sb.AppendLine(line);
+                    line = "    ";
+                }
+
+                line += entry;
+                i++;
+            }
+
+            return sb.ToString();
+        }
 
         public static readonly ParameterType World = new ParameterType(
             name: "world",
